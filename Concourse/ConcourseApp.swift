de56a4dc987232dc749d72b1e71fa5f1feca03e3
Madison L.H. Wass <931615+madisoninspace@users.noreply.github.com>
2024-09-18 23:22:04 -0400

    Initial Commit

---
+++ Concourse/ConcourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcourseApp.swift
+//  Concourse
+//
+//  Created by Madison L.H. Wass on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ConcourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
